bf59734ba1e8791856182f61c29b1a01ed2518f8
Mathias Lindholm <mathias.lindholm@gmail.com>
2023-01-15 18:33:45 +0200

    Implement input text fitting

--- src/components/AmountInput.tsx
+++ src/components/AmountInput.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Select, SelectItem, SelectPopover, useSelectState } from 'ariakit/select';
 import { Unit, WeightUnit } from 'types';
 import { ReactComponent as CheckIcon } from 'assets/check.svg';
@@ -17,17 +17,37 @@ interface Props {
 export const AmountInput: React.FC<Props> = ({ label, amount, units, currentUnit, onUnitChange, onAmountChange }) => {
   const state = useSelectState({ sameWidth: true, gutter: 4, animated: true });
   const renderName = units.find((i) => i.unit === state.value)?.name;
+  const widthRef = useRef<HTMLDivElement>(null);
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [fontSize, setFontSize] = useState(36);
+
+  useEffect(() => {
+    if (widthRef.current && inputRef.current) {
+      const tw = widthRef.current.clientWidth;
+      const iw = inputRef.current.clientWidth;
+      if (tw >= iw) {
+        setFontSize((current) => current - current / 6);
+      } else if (tw < iw - iw / 3) {
+        setFontSize(36);
+      }
+    }
+  }, [amount]);
 
   return (
     <div className={styles.container}>
       <label className={styles.label}>{label}</label>
+      <div ref={widthRef} className={styles.txtWidth} style={{ fontSize }}>
+        {amount}
+      </div>
       <input
+        ref={inputRef}
         className={styles.input}
         type="number"
         value={amount || ''}
         onChange={onAmountChange}
         min={0}
         placeholder="0"
+        style={{ fontSize }}
       />
       <Select state={state} className={styles.select}>
         {renderName || state.value}
