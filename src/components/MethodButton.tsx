cd1837d7fd46eb3ee2efe5220970cbbb14d571fd
Mathias Lindholm <mathias.lindholm@gmail.com>
2023-01-16 00:00:30 +0200

    Add missing props to MethodButton

--- src/components/MethodButton.tsx
+++ src/components/MethodButton.tsx
@@ -3,6 +3,10 @@ import { ButtonProps } from 'ariakit/button';
 import { Button } from 'components/Button';
 import styles from './MethodButton.module.css';
 
-export const MethodButton: React.FC<ButtonProps> = ({ children }) => {
-  return <Button className={styles.button}>{children}</Button>;
+export const MethodButton: React.FC<ButtonProps> = ({ children, ...rest }) => {
+  return (
+    <Button className={styles.button} {...rest}>
+      {children}
+    </Button>
+  );
 };
