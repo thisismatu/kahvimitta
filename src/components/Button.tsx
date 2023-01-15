7cc337ad118e5211dbf204ea4d13be2c2defaa75
Mathias Lindholm <mathias.lindholm@gmail.com>
2023-01-16 00:02:06 +0200

    Update Button rest props

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,15 +3,9 @@ import { Button as DefaultButton, ButtonProps } from 'ariakit/button';
 import styles from './Button.module.css';
 import clsx from 'clsx';
 
-export const Button: React.FC<ButtonProps> = ({ children, disabled, className, onClick, ...rest }) => {
+export const Button: React.FC<ButtonProps> = ({ children, className, ...rest }) => {
   return (
-    <DefaultButton
-      className={clsx(styles.button, className)}
-      as="button"
-      onClick={onClick}
-      disabled={disabled}
-      {...rest}
-    >
+    <DefaultButton className={clsx(styles.button, className)} as="button" {...rest}>
       {children}
     </DefaultButton>
   );
