c1f5905311a684485d380ee8d1c0119e28709929
Mathias Lindholm <mathias.lindholm@gmail.com>
2023-01-19 13:01:19 +0200

    Refactor install prompt logic

--- src/components/InstallPwaButton.tsx
+++ src/components/InstallPwaButton.tsx
@@ -1,52 +1,47 @@
 import React, { useEffect, useState } from 'react';
 import { useDialogState } from 'ariakit/dialog';
-import { isMobile, isIOS } from 'react-device-detect';
+import { isIOS, isBrowser } from 'react-device-detect';
 import { BeforeInstallPromptEvent } from 'types';
 import { getParam } from 'utils/misc';
 import { useLocalStorage } from 'utils/useLocalStorage';
 import { Button } from 'components/Button';
 import { IosDialog } from 'components/IosDialog';
 import { ReactComponent as DownloadIcon } from 'assets/download.svg';
-import { ReactComponent as PlusIcon } from 'assets/plus-circle.svg';
 import styles from './InstallPwaButton.module.css';
 
 export const InstallPwaButton: React.FC = () => {
-  const [iosPrompt, setIosPrompt] = useLocalStorage<boolean>('iosPrompt', true);
-  const [isInstalled, setIsInstalled] = useState<boolean>(false);
-  const [isVisible, setIsVisible] = useState<boolean>(false);
+  const [isPromptEnabled, setIsPromptEnabled] = useLocalStorage<number>('isPromptEnabled', 1);
   const [deferredPrompt, setDeferredPrompt] = useState<BeforeInstallPromptEvent>();
   const dialog = useDialogState({ animated: true });
+  const isInstalled = getParam('source') === 'pwa';
 
   useEffect(() => {
     const handleInstallPrompt = (e: BeforeInstallPromptEvent) => {
       setDeferredPrompt(e);
-      setIsVisible(true);
     };
-    if (isIOS && iosPrompt) setIsVisible(true);
-    setIsInstalled(getParam('source') === 'pwa');
     window.addEventListener('beforeinstallprompt', handleInstallPrompt);
     return () => window.removeEventListener('beforeinstallprompt', handleInstallPrompt);
   }, []);
 
   const handleClick = async () => {
-    if (isIOS && iosPrompt) return dialog.toggle();
+    if (isIOS) return dialog.toggle();
     if (!deferredPrompt) return;
     deferredPrompt.prompt();
     const { outcome } = await deferredPrompt.userChoice;
-    setDeferredPrompt(undefined);
-    setIsVisible(outcome !== 'accepted');
+    if (outcome !== 'accepted') {
+      setDeferredPrompt(undefined);
+    }
   };
 
-  if (isInstalled) return null;
-  if (!isVisible || !iosPrompt) return null;
+  if (isBrowser || isInstalled) return null;
+  if (!isPromptEnabled || deferredPrompt === undefined) return null;
 
   return (
     <>
       <Button as="button" className={styles.button} onClick={handleClick} tabIndex={-1}>
-        {isMobile ? <DownloadIcon /> : <PlusIcon />}
-        {isMobile ? 'Get' : 'Install'} app
+        <DownloadIcon /> Get app
       </Button>
-      <IosDialog state={dialog} onDontShowAgain={() => setIosPrompt(false)} />
+      <IosDialog state={dialog} onDontShowAgain={() => setIsPromptEnabled(0)} />
     </>
   );
 };
