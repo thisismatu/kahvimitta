acf19100be2d527aa456c825d1246d462477174b
Mathias Lindholm <mathias.lindholm@gmail.com>
2023-02-14 23:36:13 +0200

    Update BrewDetails layout

--- src/components/BrewDetails.tsx
+++ src/components/BrewDetails.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import { Disclosure, DisclosureContent, useDisclosureState } from 'ariakit/disclosure';
 import { Instruction } from 'types';
 import { ReactComponent as ChevronDownIcon } from 'assets/chevron-down.svg';
+import { ReactComponent as CoarsnessIcon } from 'assets/sliders.svg';
+import { ReactComponent as TemperatureIcon } from 'assets/thermometer.svg';
+import { ReactComponent as TimeIcon } from 'assets/clock.svg';
 import styles from './BrewDetails.module.css';
 import clsx from 'clsx';
 
@@ -26,23 +29,32 @@ export const BrewDetails: React.FC<Props> = ({ details }) => {
         <table className={styles.table}>
           <tbody>
             <tr>
+              <td>
+                <CoarsnessIcon />
+              </td>
               <td>Grind coarseness:</td>
               <td>{details.grind}</td>
             </tr>
             <tr>
+              <td>
+                <TemperatureIcon />
+              </td>
               <td>Water temperature:</td>
               <td>{details.temp}</td>
             </tr>
             <tr>
+              <td>
+                <TimeIcon />
+              </td>
               <td>Brew time:</td>
               <td>{details.time}</td>
             </tr>
           </tbody>
         </table>
-        <span className={styles.note}>
+        <p className={styles.note}>
           Note: Coffee scoops, tablespoons and cups are based on common european sizes. They are useful tools when you
-          don't have access to a scale.
-        </span>
+          don't have a scale.
+        </p>
       </DisclosureContent>
     </div>
   );
