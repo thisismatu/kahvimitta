b053a1ac9732bd97290362fd750ccdfb8dd1e902
Mathias Lindholm <mathias.lindholm@gmail.com>
2024-04-25 16:39:24 +0300

    Add posthog

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import { Analytics } from '@vercel/analytics/react';
 import { injectSpeedInsights } from '@vercel/speed-insights';
 import App from 'views/App';
 import { registerSW } from 'virtual:pwa-register';
+import posthog from 'posthog-js';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
@@ -19,3 +20,9 @@ registerSW({ immediate: true });
 
 // Vercel Speed Insight
 injectSpeedInsights();
+
+// PostHog without cookies
+posthog.init('phc_XinSFyU7OeaGN1Npjw8rzkVqV7u21jPyt5V30rnEDc6', {
+  api_host: 'https://eu.i.posthog.com',
+  persistence: 'memory',
+});
